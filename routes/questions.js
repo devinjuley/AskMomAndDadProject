ed9dbe70133c4106bff01de397544f81eacacc1c
devinjuley <devinjuley@gmail.com>
2021-10-19 14:45:56 -0700

    almost finished with post route for new answer on single question page

--- routes/questions.js
+++ routes/questions.js
@@ -10,7 +10,6 @@ const { loginUser, logoutUser, requireAuth } = require('../auth');
 
 router.get('/', requireAuth, asyncHandler(async (req, res) => {
   const questions = await db.Question.findAll({ include: [db.Category, db.User] })
-  // const user = await db.User.findByPk(questions.userId)
   const userId = req.session.auth.userId;
   res.render('questionFeed', { questions, userId })
 }))
@@ -28,7 +27,6 @@ router.get('/new', requireAuth, csrfProtection, asyncHandler(async (req, res) =>
 
 router.post('/new', requireAuth, csrfProtection, asyncHandler(async (req, res) => {
   const { title, content, category } = req.body
-  console.log(req.body)
   const userId = req.session.auth.userId;
   const question = await db.Question.create({ title, content, userId, categoryId: category });
   res.redirect('/questions');
@@ -40,9 +38,40 @@ router.get('/:id(\\d+)', requireAuth, csrfProtection, asyncHandler(async (req, r
   const question = await db.Question.findByPk(questionId, {
     include: [db.Category, db.User]
   })
-  // const buildQuestion = db.Question.build()
-  res.render('singleQuestion', { question, userId })
+
+  const answers = await db.Answer.findAll({
+    where: {
+      questionId: question.id
+    },
+    include: User
+  })
+
+  res.render('singleQuestion', { question, userId, answers, csrfToken: req.csrfToken() })
 }))
 
+const answerValidator = [
+  check('content')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide an answer'),
+]
+
+
+router.post('/:id(\\d+)', requireAuth, csrfProtection, answerValidator, asyncHandler(async (req, res) => {
+  const { content } = req.body;
+  const userId = req.session.auth.userId;
+  const questionId = parseInt(req.params.id, 10);
+  const answer = await db.Answer.build({ content, userId, questionId })
+  const validatorErrors = validationResult(req)
+
+  if (validatorErrors.isEmpty()) {
+    await answer.save()
+    res.redirect(`/questions/${questionId}`)
+  } else {
+    const errors = validatorErrors.array().map((error) => error.msg)
+    res.render('singleQuestion', { csrfToken: req.csrfToken, errors, content })
+  }
+
+
+}))
 
 module.exports = router;
