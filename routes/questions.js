fd7a927958db97b2596194debc564133239b67cb
Alex <poberezhskiy.a@gmail.com>
2021-10-19 10:03:05 -0700

    Added AAuthorization rrequire AUTH and restore USER added

--- routes/questions.js
+++ routes/questions.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const { csrfProtection, asyncHandler } = require('./utils');
 const bcrypt = require('bcryptjs');
 const db = require('../db/models');
-const { loginUser, logoutUser } = require('../auth');
+const { loginUser, logoutUser, requireAuth } = require('../auth');
 
 
-router.get('/', (req,res) => {
+router.get('/', requireAuth, (req,res) => {
   res.render('questionFeed')
 })
 
-router.get('/new', csrfProtection, asyncHandler(async(req, res) => {
+router.get('/new', requireAuth, csrfProtection, asyncHandler(async(req, res) => {
   const question = db.Question.build();
   const categories = await db.Category.findAll();
   res.render('newQuestion', {     // categories
