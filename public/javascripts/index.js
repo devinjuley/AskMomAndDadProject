c5680088186d7c9f7968afe8163f3705d47292af
devinjuley <devinjuley@gmail.com>
2021-10-20 12:34:25 -0700

    fixed delete bug

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,39 +1,40 @@
-// window.addEventListener("load", (event)=>{
-//     console.log("hello from javascript!")
-// })
 
-window.onload = () => {
+console.log('this is a test')
+const buttons = document.querySelectorAll('.delete-btn')
 
-const deleteButton = document.querySelectorAll('.delete-btn')
+for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    button.addEventListener('click', async (e) => {
+        e.preventDefault();
+        const questionId = e.target.id.split('-')[2]
+        const res = await fetch(`/questions/${questionId}`, {
+            method: 'DELETE'
+        })
 
-// for (let i = 0; i < buttons.length; i++) {
-//     const button = buttons[i];
-//     button.addEventListener('click', async (e) => {
-//         e.preventDefault();
-//         const questionId = e.target.id.split('-')[1]
-//         const res = await fetch(`/questions/${questionId}`, {
-//             method: 'DELETE'
-//         })
-
-//         const data = await res.json()
-//         if (data.message === "Success") {
-//             const container = document.querySelector(`#question-container-${questionId}`)
-//             container.remove()
-//         }
-//     })
-// }
-
-const deleteButton = document.createElement('button');
-deleteButton.className = "delete-button";
-deleteButton.style.marginLeft = "45px";
-deleteButton.innerText = 'Delete';
-deleteButton.addEventListener('click', e => {
-    // Remove comment from HTML DOM
-    newCommentContainer.remove();
-});
-
-
-
-
+        const data = await res.json()
+        if (data.message === "Success") {
+            const container = document.querySelector(`#question-container-${questionId}`)
+            container.remove()
+        }
+    })
 
 }
+
+
+
+
+
+// const deleteButton = document.createElement('button');
+// deleteButton.className = "delete-button";
+// deleteButton.style.marginLeft = "45px";
+// deleteButton.innerText = 'Delete';
+// deleteButton.addEventListener('click', e => {
+//     // Remove comment from HTML DOM
+//     newCommentContainer.remove();
+// });
+
+
+
+
+
+// }
