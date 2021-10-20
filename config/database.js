0e2f69c5c1ff00b54c86c5a6ae4ad07c71def664
devinjuley <devinjuley@gmail.com>
2021-10-20 09:04:53 -0700

    fixed database.js

--- config/database.js
+++ config/database.js
@@ -2,6 +2,17 @@ const {
   db: { username, password, database, host },
 } = require('./index');
 
+// module.exports = {
+//   development: {
+//     username,
+//     password,
+//     database,
+//     host,
+//     dialect: 'postgres',
+//     seederStorage: 'sequelize',
+//   },
+// };
+
 module.exports = {
   development: {
     username,
@@ -9,6 +20,10 @@ module.exports = {
     database,
     host,
     dialect: 'postgres',
-    seederStorage: 'sequelize',
   },
+  production: {
+    use_env_variable: 'postgres://nqsojyzluolvzi:37ad1ea64f15370ff1ca50b0ce32b9ff158f349d76fd618544db87bc07f5cd34@ec2-44-195-240-222.compute-1.amazonaws.com:5432/d6ceduh3a1e9lp',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+  }
 };
