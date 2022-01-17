f54133d58247a62b0992c5d3ec22c4c734c551e3
devinjuley <devinjuley@gmail.com>
2022-01-17 12:40:51 -0800

    added pictures to Questions

--- db/models/question.js
+++ db/models/question.js
@@ -3,13 +3,14 @@ module.exports = (sequelize, DataTypes) => {
   const Question = sequelize.define('Question', {
     title: DataTypes.STRING,
     content: DataTypes.TEXT,
+    imageURL: DataTypes.STRING,
     userId: DataTypes.INTEGER,
     categoryId: DataTypes.INTEGER
   }, {});
   Question.associate = function (models) {
-    Question.belongsTo(models.User, { foreignKey: 'userId'})
-    Question.belongsTo(models.Category, { foreignKey: 'categoryId'})
-    Question.hasMany(models.Answer, { foreignKey: 'questionId', onDelete: 'CASCADE', hooks: true})
+    Question.belongsTo(models.User, { foreignKey: 'userId' })
+    Question.belongsTo(models.Category, { foreignKey: 'categoryId' })
+    Question.hasMany(models.Answer, { foreignKey: 'questionId', onDelete: 'CASCADE', hooks: true })
   };
   return Question;
 };
