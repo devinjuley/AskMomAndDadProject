67dbecb2825233323be1eded606aa875e85503b8
devinjuley <devinjuley@gmail.com>
2022-01-17 12:50:43 -0800

    added new users seeder data

--- db/seeders/20211020210109-userSeeder.js
+++ db/seeders/20211020210109-userSeeder.js
@@ -1,10 +1,61 @@
 'use strict';
+const faker = require('faker');
+const bcrypt = require('bcryptjs');
+
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
 
     return queryInterface.bulkInsert('Users', [
-      { username: 'devin', email: 'devinjuley@gmail.com', hashedPassword: 'something', createdAt: new Date(), updatedAt: new Date() }
+      {
+        username: 'John',
+        email: 'john@john.com',
+        hashedPassword: bcrypt.hashSync('password'),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'Tammy',
+        email: 'tammy@tammy.com',
+        hashedPassword: bcrypt.hashSync('password'),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'Phil',
+        email: 'phil@phil.com',
+        hashedPassword: bcrypt.hashSync('password'),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'Sarah',
+        email: 'sarah@sarah.com',
+        hashedPassword: bcrypt.hashSync('password'),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'Todd',
+        email: 'todd@todd.com',
+        hashedPassword: bcrypt.hashSync('password'),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'Orlando',
+        email: 'orlando@orlando.com',
+        hashedPassword: bcrypt.hashSync('password'),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'Alexis',
+        email: 'alexis@alexis.com',
+        hashedPassword: bcrypt.hashSync('password'),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
     ], {});
 
   },
